328944eb910c1a23a9743a717801b372c68886e0
Michael Spohn <mikeLspohn@gmail.com>
2018-11-17 20:35:45 -0600

    Let the world know whats up!

--- src/PokeList.tsx
+++ src/PokeList.tsx
@@ -2,11 +2,22 @@ import React, { useReducer, useEffect } from "react"
 import { unstable_createResource as createResource } from "react-cache"
 import fetchPokemon from "./fetchPokemon"
 
-// New experimental async rendering data fetcher!
-const myPokemon = createResource(fetchPokemon)
-
+// Component Interfaces
 interface PokeListProps {
   search: string
+}
+
+// Pokemon Interfaces (you can see how you can build up composable interfaces)
+interface Pokemon {
+  id: number
+  number: number
+  name: string
+  attacks: PokemonAttacks
+}
+
+interface PokemonAttacks {
+  special: Array<PokemonAttack>
+  fast: Array<PokemonAttack>
 }
 
 interface PokemonAttack {
@@ -15,18 +26,16 @@ interface PokemonAttack {
   damage: string
 }
 
-interface PokemonAttacks {
-  special: Array<PokemonAttack>
-  fast: Array<PokemonAttack>
-}
-
-interface Pokemon {
-  id: number
-  number: number
-  name: string
-  attacks: PokemonAttacks
-}
-
+// Reducer Interfaces
+//
+// The awesome thing about typescript wtih reducers
+// is there's no chance of missing a `case` in a switch
+// over these types, and you can be confident your reducer
+// can't be called with improper action types and that it will
+// always return the correct state shape
+// Besides actual logic (math, reordering so on) everything is really
+// safe with the type checking and it elminates a lot of need for
+// simple tests like testing that an actionCreator returns a valid action type
 type actionType = "ADD_POKEMON" | "POKEMON_NOT_FOUND"
 
 interface action {
@@ -39,8 +48,15 @@ type reducerState<T> = {
   error: string
 }
 
+// New experimental async rendering data fetcher!
+const myPokemon = createResource(fetchPokemon)
 const initialState: reducerState<Pokemon> = { pokemon: [], error: "" }
 
+// The real advantages of the `reducer` pattern really come out with
+// static typing in place. We can have much more assurance that we have a pure
+// data-in data-out function that always yields predicatable results.
+// And we can also feel more secure in our own ability to not mess up when dispatch an
+// action name "ADD_NEW_ITEM_TO_COMPENSABLE_DUTY" (and elminates the need to set those up as constants)
 const pokemonReducer = (
   state = initialState,
   { type, payload }: action
@@ -65,23 +81,31 @@ export default function PokeList({ search }: PokeListProps) {
 
   useEffect(
     () => {
-      console.log("useEffect hook fired")
+      // if our search fetched a pokemon successfully
+      // and the pokemon is not already in our list then dispatch addPokemon
+      // otherwise set the error state to show a not found message
       if (
         pokemonFromSearch &&
         !pokemonList.pokemon.find(
           pokemon => pokemon.id === pokemonFromSearch.id
         )
       ) {
-        console.log("add pokemon fired")
         dispatch({ type: "ADD_POKEMON", payload: pokemonFromSearch })
       } else {
-        console.log("pokemon not found fired")
         dispatch({ type: "POKEMON_NOT_FOUND" })
       }
     },
+    // 2nd param to useEffect is what tells it when it should refire
+    // so whenever pokemonFromSearch changes, this code should refire on that render
     [pokemonFromSearch]
   )
 
+  // with `pokemonList` being typed (return type from reducer function) we can
+  // be sure that nothing in render should ever fail because of trying to access
+  // a property of `undefined` (cannot call length of undefined)
+  // and if the data shape for `Pokemon` ever changes, it will be easy to spot
+  // what and where exactly in the UI that props need to be updated accordingly so that could
+  // never be something that simply got looked over
   return (
     <>
       {pokemonList.error && <h1>{pokemonList.error}</h1>}
